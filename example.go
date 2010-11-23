9443abf461a6c4830727c79636c6568313212a55
Gustavo Niemeyer <gustavo@niemeyer.net>
2010-11-23 13:46:50 -0200

    Adding example.go.

---
+++ example.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "gozk"
+)
+
+func main() {
+    zk, session, err := gozk.Init("localhost:2181", 5000)
+    if err != nil {
+        println("Couldn't connect: " + err.String())
+    } else {
+        defer zk.Close()
+
+        // Wait for connection.
+        event := <-session
+        if event.State != gozk.STATE_CONNECTED {
+            println("Couldn't connect")
+        } else {
+            _, err := zk.Create("/counter", "0", 0,
+                                gozk.WorldACL(gozk.PERM_ALL))
+            if err != nil {
+                println(err.String())
+            } else {
+                println("Created!")
+            }
+        }
+
+    }
+}
