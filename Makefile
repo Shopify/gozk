37f9d4851e9c7c4f87a73115d01d5957e7b3ee7f
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-01-12 10:41:12 -0200

    Improved Makefile so that building statically is possible with STATIC=1, and ZKROOT is handled internally.

--- Makefile
+++ Makefile
@@ -8,10 +8,29 @@ CGOFILES=\
 CGO_OFILES=\
 	helpers.o\
 
-CGO_LDFLAGS+=-lzookeeper_mt
+ifdef ZKROOT
+LIBDIR=$(ZKROOT)/src/c/.libs
+LD_LIBRARY_PATH:=$(LIBDIR):$(LD_LIBRARY_PATH)
+CGO_CFLAGS+=-I$(ZKROOT)/src/c/include -I$(ZKROOT)/src/c/generated
+CGO_LDFLAGS+=-L$(LIBDIR)
+else
+LIBDIR=/usr/lib
+CGO_CFLAGS+=-I/usr/include/zookeeper
+endif
 
-#CGO_LDFLAGS=-lm -lpthread
-#CGO_OFILES=helpers.o $(shell ls asd/*.o)
+ifndef STATIC
+CGO_LDFLAGS+=-lzookeeper_mt
+else
+CGO_LDFLAGS+=-lm -lpthread
+CGO_OFILES+=$(wildcard _lib/*.o)
+
+all: package
+_lib:
+	@mkdir -p _lib
+	cd _lib && ar x $(LIBDIR)/libzookeeper_mt.a
+
+_cgo_defun.c: _lib
+endif
 
 include $(GOROOT)/src/Make.pkg
 
--- example.go
+++ example.go
@@ -8,22 +8,23 @@ func main() {
     zk, session, err := gozk.Init("localhost:2181", 5000)
     if err != nil {
         println("Couldn't connect: " + err.String())
+        return
+    }
+
+    defer zk.Close()
+
+    // Wait for connection.
+    event := <-session
+    if event.State != gozk.STATE_CONNECTED {
+        println("Couldn't connect")
+        return
+    }
+
+    _, err := zk.Create("/counter", "0", 0, gozk.WorldACL(gozk.PERM_ALL))
+    if err != nil {
+        println(err.String())
     } else {
-        defer zk.Close()
-
-        // Wait for connection.
-        event := <-session
-        if event.State != gozk.STATE_CONNECTED {
-            println("Couldn't connect")
-        } else {
-            _, err := zk.Create("/counter", "0", 0,
-                                gozk.WorldACL(gozk.PERM_ALL))
-            if err != nil {
-                println(err.String())
-            } else {
-                println("Created!")
-            }
-        }
-
+        println("Created!")
     }
 }
+
